dd62d34d2077b03163a56389d229ff7414e96f75
Oxana Odegova <oxyapple@yandex.ru>
2020-12-18 12:33:04 +0100

    states 2

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  constructor (props) {
-    super (props);
+  state = {lat: null, errorMessage: ''};
 
-    this.state = {lat: null, errorMessage: ''};
-
+  //better place to do one time loading
+  componentDidMount () {
     window.navigator.geolocation.getCurrentPosition (
-      position => {
-        this.setState ({lat: position.coords.latitude});
-      },
-      err => {
-        this.setState ({errorMessage: err.message});
-      }
+      position => this.setState ({lat: position.coords.latitude}),
+      err => this.setState ({errorMessage: err.message})
     );
+  }
+
+  renderContent () {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    } else {
+      return <Spinner message="Please accept location request" />;
+    }
   }
 
   //react says we have to define render!!
   render () {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    } else if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
-    } else {
-      return <div>Loading ...</div>;
-    }
+    return <div className="border red">{this.renderContent ()}</div>;
   }
 }
 
